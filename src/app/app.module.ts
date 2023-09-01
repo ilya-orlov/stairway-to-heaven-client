e75187d5b8d22c1e5786202f02f730bc4e91aa85
Ilya Orlov <orloveagle@mail.ru>
2023-09-02 00:24:27 +0300

    chore: main page routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TuiAppBarModule } from '@taiga-ui/addon-mobile';
+import { TuiRootModule } from '@taiga-ui/core';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, TuiAppBarModule, TuiRootModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MainRoutingModule } from './main-routing.module';
-import { MainComponent } from './main/main.component';
+import { MainComponent } from './main.component';
+import { TuiAppBarModule } from '@taiga-ui/addon-mobile';
+import { TuiButtonModule } from '@taiga-ui/core';
 
 @NgModule({
   declarations: [MainComponent],
-  imports: [CommonModule, MainRoutingModule],
+  imports: [CommonModule, MainRoutingModule, TuiAppBarModule, TuiButtonModule],
 })
 export class MainModule {}
