6f21a80efa9bb4a004a7f14c47a1951f0bb84feb
Ilya Orlov <orloveagle@mail.ru>
2023-09-02 01:08:25 +0300

    chore: icon providers

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -4,10 +4,20 @@ import { CommonModule } from '@angular/common';
 import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main.component';
 import { TuiAppBarModule } from '@taiga-ui/addon-mobile';
-import { TuiButtonModule } from '@taiga-ui/core';
+import { TUI_BUTTON_OPTIONS, TuiButtonModule } from '@taiga-ui/core';
 
 @NgModule({
   declarations: [MainComponent],
   imports: [CommonModule, MainRoutingModule, TuiAppBarModule, TuiButtonModule],
+  providers: [
+    {
+      provide: TUI_BUTTON_OPTIONS,
+      useValue: {
+        appearance: 'flat',
+        size: 'm',
+        shape: 'rounded',
+      },
+    },
+  ],
 })
 export class MainModule {}
